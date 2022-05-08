d73a0624c12cc3ced5258036b2afa91ced92f694
21seu <1693376841@qq.com>
2022-05-08 19:23:47 +0800

    导航菜单功能实现

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
+import Test1 from '../views/Test1.vue'
+import Test2 from '../views/Test2.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -9,28 +11,41 @@ Vue.use(VueRouter)
 Vue.use(ElementUI);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  },
-  /*{
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
-  }*/
+    {
+        path: '/',
+        name: 'Login',
+        component: Login,
+        hidden:true
+    },
+    {
+        path: '/home',
+        name: '导航1',
+        component: Home,
+        children: [
+            {
+                path: '/test1',
+                name: '选项1',
+                component: Test1
+            },
+            {
+                path: '/test2',
+                name: '选项2',
+                component: Test2
+            }
+        ]
+    },
+    /*{
+      path: '/about',
+      name: 'About',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
+    }*/
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,46 @@
 <template>
   <div>
-    home
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu router>
+          <el-submenu index="1"
+                      v-for="(item,index) in this.$router.options.routes"
+                      :key="index"
+                      v-if="!item.hidden">
+            <template slot="title"><i class="el-icon-location"></i>{{item.name}}</template>
+            <el-menu-item index="children.path"
+                          v-for="(children,indexj) in item.children">{{ children.name }}
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
+
 </template>
 
 <script>
+
 export default {
-  name: "Home"
+  name: 'Home',
+
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {
+    // menuClick(index) {
+    //   this.$router.push(index);
+    // }
+  },
+  created() {
+  }
 }
 </script>
-
-<style scoped>
+<style scoped lang="less">
 
 </style>
