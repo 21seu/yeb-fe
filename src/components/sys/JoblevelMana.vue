fa12400026efa21b9037199f7d44ada10a3aa8d4
21seu <1693376841@qq.com>
2022-05-11 23:57:23 +0800

    职称管理批量删除功能

--- src/components/sys/JoblevelMana.vue
+++ src/components/sys/JoblevelMana.vue
@@ -19,7 +19,12 @@
           size="small"
           stripe
           border
-          style="width: 70%">
+          @selection-change="handlerSelectionChange"
+          style="width: 80%">
+        <el-table-column
+            type="selection"
+            width="55">
+        </el-table-column>
         <el-table-column
             prop="id"
             label="编号"
@@ -58,6 +63,10 @@
         </el-table-column>
       </el-table>
     </div>
+    <el-button size="small" style="margin-top: 8px" type="danger"
+               :disabled="this.multipleSelection.length == 0"
+               @click="deleteMany">批量删除
+    </el-button>
     <el-dialog
         title="编辑职称"
         :visible.sync="dialogVisible"
@@ -134,10 +143,36 @@ export default {
         titleLevel: '',
         enabled: false,
 
-      }
+      },
+      multipleSelection: []
     }
   },
   methods: {
+    deleteMany() {
+      this.$confirm('此操作将永久删除【' + this.multipleSelection.length + '】条职称, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        let ids = '?';
+        this.multipleSelection.forEach(item => {
+          ids += 'ids=' + item.id + '&'
+        })
+        this.deleteRequest('/sysytem/basic/joblevel/' + ids).then(resp => {
+          if (resp) {
+            this.initJls();
+          }
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    handlerSelectionChange(val) {
+      this.multipleSelection = val;
+    },
     doUpdate() {
       this.putRequest('/sysytem/basic/joblevel/', this.updateJl).then(resp => {
         if (resp) {
