1f7661dcbc26bdf6ce64d9c80c83ad4516547312
21seu <1693376841@qq.com>
2022-05-11 21:47:45 +0800

    职位管理接口联调

--- src/components/sys/PosMana.vue
+++ src/components/sys/PosMana.vue
@@ -4,14 +4,19 @@
       <el-input class="addPosInput" size="small"
                 placeholder="添加职位..."
                 suffix-icon="el-icon-plus"
+                @keydown.enter.native="addPosition"
                 v-model="pos.name">
       </el-input>
-      <el-button type="primary" icon="el-icon-plus" size="small">添加</el-button>
+      <el-button type="primary" icon="el-icon-plus" size="small" @click="addPosition">添加</el-button>
     </div>
     <div class="posManaMain">
       <el-table border stripe size="small"
                 :data="positions"
-                style="width: 100%">
+                style="width: 70%">
+        <el-table-column
+            type="selection"
+            width="55">
+        </el-table-column>
         <el-table-column
             prop="id"
             label="编号"
@@ -28,9 +33,18 @@
             width="200">
         </el-table-column>
         <el-table-column
-            prop="enabled"
-            label="是否启用"
-            width="120">
+            label="操作">
+          <template slot-scope="scope">
+            <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">编辑
+            </el-button>
+            <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">删除
+            </el-button>
+          </template>
         </el-table-column>
       </el-table>
     </div>
@@ -47,6 +61,52 @@ export default {
       },
       positions: []
     }
+  },
+  methods: {
+    addPosition() {
+      if (!this.pos.name) {
+        this.$message.error("职位名称不能为空")
+      } else {
+        this.postRequest('/system/basic/pos/', this.pos).then(resp => {
+          if (resp) {
+            this.initPositions();
+            this.pos.name = '';
+          }
+        })
+      }
+    },
+    handleEdit(index, data) {
+
+    },
+    handleDelete(index, data) {
+      // this.$confirm('此操作将永久删除【' + data.name + '】职位, 是否继续?', '提示', {
+      this.$confirm('此操作将永久删除【' + data.name + '】职位, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.deleteRequest('/system/basic/pos/' + data.id).then(resp => {
+          if (resp) {
+            this.initPositions();
+          }
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    initPositions() {
+      this.getRequest('/system/basic/pos/').then(resp => {
+        if (resp) {
+          this.positions = resp;
+        }
+      })
+    }
+  },
+  mounted() {
+    this.initPositions();
   }
 }
 </script>
@@ -56,7 +116,8 @@ export default {
   width: 300px;
   margin-right: 8px;
 }
-.posManaMain{
+
+.posManaMain {
   margin-top: 10px;
 }
 </style>
