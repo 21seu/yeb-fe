3e35fd0c7a0de5e43105a08a6cebde824cb3dbf4
21seu <1693376841@qq.com>
2022-05-11 23:12:21 +0800

    职称管理页面设计

--- src/components/sys/JoblevelMana.vue
+++ src/components/sys/JoblevelMana.vue
@@ -1,10 +1,81 @@
 <template>
-
+  <div>
+    <div>
+      <el-input style="width:300px" size="small" v-model="jl.name" placeholder="添加职称等级..."
+                prefix-icon="el-icon-plus"></el-input>
+      <el-select size="small" v-model="jl.titleLevel" placeholder="职称等级" style="margin-left: 6px;margin-right: 6px">
+        <el-option
+            v-for="item in titleLevels"
+            :key="item"
+            :label="item"
+            :value="item">
+        </el-option>
+      </el-select>
+      <el-button type="primary" size="small" icon="el-icon-plus">添加</el-button>
+    </div>
+    <div style="margin-top: 10px">
+      <el-table
+          :data="jls"
+          size="small"
+          stripe
+          border
+          style="width: 70%">
+        <el-table-column
+            prop="id"
+            label="编号"
+            width="55">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="职称名称"
+            width="150">
+        </el-table-column>
+        <el-table-column
+            prop="address"
+            label="职称等级"
+            width="150">
+        </el-table-column>
+        <el-table-column
+            prop="createDate"
+            label="创建日期"
+            width="150">
+        </el-table-column>
+        <el-table-column
+            prop="enabled"
+            label="是否启用"
+            width="150px">
+        </el-table-column>
+        <el-table-column
+            label="操作">
+          <template slot-scope="scope">
+            <el-button size="small">编辑</el-button>
+            <el-button size="small" type="danger">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "JoblevelMana"
+  name: "JoblevelMana",
+  data() {
+    return {
+      jl: {
+        name: '',
+        titleLevel: ''
+      },
+      titleLevels: [
+        '正高级',
+        '副高级',
+        '中级',
+        '初级',
+        '员级'
+      ],
+      jls: []
+    }
+  }
 }
 </script>
 
--- src/views/sys/SysBasic.vue
+++ src/views/sys/SysBasic.vue
@@ -28,7 +28,7 @@ export default {
   },
   data() {
     return {
-      activeName: 'PosMana'
+      activeName: 'JoblevelMana'
     }
   }
 }
