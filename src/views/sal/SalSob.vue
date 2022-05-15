563a7969187b3f8f21065daa6c2999abe59fd8b8
21seu <1693376841@qq.com>
2022-05-15 19:50:55 +0800

    添加工资账套页面设计

--- src/views/sal/SalSob.vue
+++ src/views/sal/SalSob.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div style="display: flex;justify-content: space-between">
-      <el-button size="small" type="primary" icon="el-icon-plus">添加工资账套</el-button>
+      <el-button size="small" type="primary" icon="el-icon-plus" @click="showAddSalaryView">添加工资账套</el-button>
       <el-button size="small" type="success" icon="el-icon-refresh"></el-button>
     </div>
     <div style="margin-top: 10px">
@@ -87,11 +87,28 @@
         </el-table-column>
         <el-table-column
             label="操作">
-          <el-button type="primary">编辑</el-button>
-          <el-button type="danger">删除</el-button>
+          <el-button size="small" type="primary">编辑</el-button>
+          <el-button size="small" type="danger">删除</el-button>
         </el-table-column>
       </el-table>
     </div>
+
+    <el-dialog
+        title="添加工资账套"
+        :visible.sync="dialogVisible"
+        width="50%">
+      <div style="display: flex;justify-content: space-around;align-items: center">
+        <el-steps direction="vertical" :active="activeItemIndex">
+          <el-step :title="itemName" v-for="(itemName,index) in salaryItemName" :key="index"></el-step>
+        </el-steps>
+        <el-input :placeholder="'请输入'+itemName+'...'" v-for="(itemName,index) in salaryItemName"
+                  :key="index" v-show="activeItemIndex==index" style="width: 200px"></el-input>
+      </div>
+      <span slot="footer" class="dialog-footer">
+    <el-button size="small" @click="preStep">{{ activeItemIndex == 10 ? '取消' : '上一步' }}</el-button>
+    <el-button size="small" type="primary" @click="nextStep">{{ activeItemIndex == 10 ? '完成' : '下一步' }}</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -100,10 +117,45 @@ export default {
   name: "SalSob",
   data() {
     return {
+      activeItemIndex: 0,
       salaries: [],
+      dialogVisible: false,
+      salaryItemName: [
+        '账套名称',
+        '基本工资',
+        '交通补助',
+        '午餐补助',
+        '奖金',
+        '养老金比率',
+        '养老金基数',
+        '医疗保险比率',
+        '医疗保险基数',
+        '公积金比率',
+        '公积金基数'
+      ],
     }
   },
   methods: {
+    preStep() {
+      if (this.activeItemIndex == 0) {
+        return;
+      } else if (this.activeItemIndex == 10) {
+        this.dialogVisible = false;
+        return;
+      }
+      this.activeItemIndex--;
+    },
+    nextStep() {
+      if (this.activeItemIndex == 10) {
+        alert(1)
+        return;
+      }
+      this.activeItemIndex++;
+    },
+    showAddSalaryView() {
+      this.activeItemIndex = 0;
+      this.dialogVisible = true;
+    },
     initSalaries() {
       this.getRequest('/salary/sob/').then(resp => {
         console.log(resp)
