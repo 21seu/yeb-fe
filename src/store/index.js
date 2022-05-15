ca25b8e3e9f5976715496a10f22fb915455dec63
21seu <1693376841@qq.com>
2022-05-15 21:10:27 +0800

    聊天页面侧边栏修改

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {getRequest} from "../utils/api";
 
 Vue.use(Vuex)
 
@@ -10,61 +11,44 @@ const now = new Date();
 const store = new Vuex.Store({
     state: {
         routes: [],
-        sessions:[{
-            id:1,
-            user:{
-                name:'示例介绍',
-                img:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic_source%2F33%2F6d%2F82%2F336d82ec4a3fc5cf5add5c347946452e.jpg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1655086471&t=4c9b5b892ecec7b1439d6d0c273a05f3'
-            },
-            messages:[{
-                content:'Hello，这是一个基于Vue + Vuex + Webpack构建的简单chat示例，聊天记录保存在localStorge, 有什么问题可以通过Github Issue问我。',
-                date:now
-            },{
-                content:'项目地址(原作者): https://github.com/coffcer/vue-chat',
-                date:now
-            },{
-                content:'本项目地址(重构): https://github.com/is-liyiwei',
-                date:now
-            }]
-        },{
-            id:2,
-            user:{
-                name:'webpack',
-                img:'https://img1.baidu.com/it/u=1071197378,3142915249&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500'
-            },
-            messages:[{
-                content:'Hi，我是webpack哦',
-                date:now
-            }]
-        }],
-        currentSessionId:1,
-        filterKey:''
+        sessions: [],
+        admins: [],
+        currentSessionId: -1,
+        filterKey: ''
     },
     mutations: {
         initRoutes(state, data) {
             state.routes = data;
         },
-        changeCurrentSessionId (state,id) {
+        changeCurrentSessionId(state, id) {
             state.currentSessionId = id;
         },
-        addMessage (state,msg) {
-            state.sessions[state.currentSessionId-1].messages.push({
-                content:msg,
+        addMessage(state, msg) {
+            state.sessions[state.currentSessionId - 1].messages.push({
+                content: msg,
                 date: new Date(),
-                self:true
+                self: true
             })
         },
-        INIT_DATA (state) {
-            let data = localStorage.getItem('vue-chat-session');
-            //console.log(data)
-            if (data) {
-                state.sessions = JSON.parse(data);
-            }
+        INIT_DATA(state) {
+            //浏览器本地的历史聊天记录
+            // let data = localStorage.getItem('vue-chat-session');
+            // //console.log(data)
+            // if (data) {
+            //     state.sessions = JSON.parse(data);
+            // }
+        },
+        INIT_ADMINS(state, data) {
+            state.admins = data;
         }
     },
     actions: {
-        initData (context) {
-            context.commit('INIT_DATA')
+        initData(context) {
+            getRequest('/chat/admin').then(resp => {
+                if (resp) {
+                    context.commit('INIT_ADMINS', resp)
+                }
+            })
         }
     },
     modules: {}
@@ -72,11 +56,11 @@ const store = new Vuex.Store({
 
 store.watch(function (state) {
     return state.sessions
-},function (val) {
+}, function (val) {
     console.log('CHANGE: ', val);
     localStorage.setItem('vue-chat-session', JSON.stringify(val));
-},{
-    deep:true/*这个貌似是开启watch监测的判断,官方说明也比较模糊*/
+}, {
+    deep: true/*这个貌似是开启watch监测的判断,官方说明也比较模糊*/
 })
 
 
