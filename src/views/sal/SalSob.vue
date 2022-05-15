e7371cb2c0d1ae6eaed0f5663a99caa9401e66f0
21seu <1693376841@qq.com>
2022-05-15 19:36:57 +0800

    工资账套页面设计

--- src/views/sal/SalSob.vue
+++ src/views/sal/SalSob.vue
@@ -1,10 +1,121 @@
 <template>
-
+  <div>
+    <div style="display: flex;justify-content: space-between">
+      <el-button size="small" type="primary" icon="el-icon-plus">添加工资账套</el-button>
+      <el-button size="small" type="success" icon="el-icon-refresh"></el-button>
+    </div>
+    <div style="margin-top: 10px">
+      <el-table
+          :data="salaries"
+          border
+          stripe>
+        <el-table-column
+            type="selection"
+            width="40">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="账套名称"
+            width="120">
+        </el-table-column>
+        <el-table-column
+            prop="basicSalary"
+            label="基本工资"
+            width="120">
+        </el-table-column>
+        <el-table-column
+            prop="trafficSalary"
+            label="交通补助"
+            width="70">
+        </el-table-column>
+        <el-table-column
+            prop="lunchSalary"
+            label="午餐补助"
+            width="70">
+        </el-table-column>
+        <el-table-column
+            prop="bonus"
+            label="奖金"
+            width="70">
+        </el-table-column>
+        <el-table-column
+            prop="createDate"
+            label="启用时间"
+            width="100">
+        </el-table-column>
+        <el-table-column
+            label="养老金"
+            align="center">
+          <el-table-column
+              prop="pensionPer"
+              label="比率"
+              width="70">
+          </el-table-column>
+          <el-table-column
+              prop="pensionBase"
+              label="基数"
+              width="70">
+          </el-table-column>
+        </el-table-column>
+        <el-table-column
+            label="医疗保险"
+            align="center">
+          <el-table-column
+              prop="medicalPer"
+              label="比率"
+              width="70">
+          </el-table-column>
+          <el-table-column
+              prop="medicalBase"
+              label="基数"
+              width="70">
+          </el-table-column>
+        </el-table-column>
+        <el-table-column
+            label="公积金"
+            align="center">
+          <el-table-column
+              prop="accumulationFundPer"
+              label="比率"
+              width="70">
+          </el-table-column>
+          <el-table-column
+              prop="accumulationFundBase"
+              label="基数"
+              width="70">
+          </el-table-column>
+        </el-table-column>
+        <el-table-column
+            label="操作">
+          <el-button type="primary">编辑</el-button>
+          <el-button type="danger">删除</el-button>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "SalSob"
+  name: "SalSob",
+  data() {
+    return {
+      salaries: [],
+    }
+  },
+  methods: {
+    initSalaries() {
+      this.getRequest('/salary/sob/').then(resp => {
+        console.log(resp)
+        if (resp) {
+          this.salaries = resp;
+        }
+      })
+    }
+  },
+  mounted() {
+    this.initSalaries();
+  }
 }
 </script>
 
